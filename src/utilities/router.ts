3db0d1184f8dc47e1f92cedad8b2ff6bcefa3b34
localhousee <ananditodwiky666@gmail.com>
2022-10-10 17:28:55 +0700

    fix valk type image doesnt show

--- src/utilities/router.ts
+++ src/utilities/router.ts
@@ -1,18 +1,27 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "@/Pages/Valkyries/Index.vue";
+import About from "@/Pages/About.vue";
+import Credits from "@/Pages/Credits.vue";
+import SubmitBuild from "@/Pages/SubmitBuild.vue";
+import CheckBuild from "@/Pages/CheckBuild.vue";
+import Login from "@/Pages/Auth/Login.vue";
+import Details from "@/Pages/Valkyries/Details.vue";
+import Admin from "@/Pages/Admin/Index.vue";
+import AddValkyrie from "@/Pages/Admin/Valkyrie/Create.vue";
+import EditValkyrie from "@/Pages/Admin/Valkyrie/Update.vue";
 import { auth } from "@/utilities/auth";
 
 const routes = [
   { path: "/", name:"valkyries.index", component: Index },
-  { path: "/about", name:"about", component: () => import ("@/Pages/About.vue") },
-  { path: "/credits", name:"credits", component: () => import("@/Pages/Credits.vue") },
-  { path: "/submit-build", name: "submit.build", component: () => import("@/Pages/SubmitBuild.vue") },
-  { path: "/check-build/:id?", name: "check.build", component: () => import("@/Pages/CheckBuild.vue") },
-  { path: "/login", name: "login", component: () => import("@/Pages/Auth/Login.vue") },
-  { path: "/valkyries/:name", name: "valkyries.details", component: () => import ("@/Pages/Valkyries/Details.vue") },
-  { path: "/admin", name: "admin", component: () => import("@/Pages/Admin/Index.vue") },
-  { path: "/admin/valkyries/create", name: "admin.valkyries.create", component: () => import("@/Pages/Admin/Valkyrie/Create.vue") },
-  { path: "/admin/valkyries/:name", name: "admin.valkyries.edit", component: () => import("@/Pages/Admin/Valkyrie/Update.vue") },
+  { path: "/about", name:"about", component: About },
+  { path: "/credits", name: "credits", component: Credits },
+  { path: "/submit-build", name: "submit.build", component: SubmitBuild },
+  { path: "/check-build/:id?", name: "check.build", component: CheckBuild },
+  { path: "/login", name: "login", component: Login },
+  { path: "/valkyries/:name", name: "valkyries.details", component: Details },
+  { path: "/admin", name: "admin", component: Admin },
+  { path: "/admin/valkyries/create", name: "admin.valkyries.create", component: AddValkyrie },
+  { path: "/admin/valkyries/:name", name: "admin.valkyries.edit", component: EditValkyrie },
 ];
 
 const router = createRouter({

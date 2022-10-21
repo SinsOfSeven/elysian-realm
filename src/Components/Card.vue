b2ad125c785a9f0fafe7d8fccf59b29501f9cb21
localhousee <ananditodwiky666@gmail.com>
2022-10-21 14:39:32 +0700

    update index images

--- src/Components/Card.vue
+++ src/Components/Card.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { Valkyrie } from "@/utilities/types";
 
 type Props = { valkyrie: Valkyrie };
-defineProps<Props>();
+const props = defineProps<Props>();
 
-let loading = ref<Boolean>(true);
-let isLoaded = ref<Boolean>(false);
+let loading = ref(true);
+let isLoaded = ref(false);
 
 const loaded = () => { loading.value = false; isLoaded.value = true };
+
+const image = computed(() => {
+  const nameArray = props.valkyrie.image.split(".");
+  return `${nameArray[0]}-min.${nameArray[1]}`;
+});
 </script>
 <template>
   <router-link :to="`/valkyries/${valkyrie.slug}`">
@@ -17,7 +22,7 @@ const loaded = () => { loading.value = false; isLoaded.value = true };
         class="aspect-w-1 aspect-h-1 flex min-h-12 lg:h-12 w-full flex-col items-center overflow-hidden bg-transparent rounded-tr-xl transition delay-150 ease-in-out group-hover:opacity-75">
         <img v-if="loading" class="m-auto w-full h-full bg-transparent" src="/assets/loading.gif"
           alt="loading.." />
-        <img v-show="isLoaded" :src="valkyrie.image" :alt="valkyrie.name" :class="valkyrie.position" @load="loaded"
+        <img v-show="isLoaded" :src="image" :alt="valkyrie.name" :class="valkyrie.position" @load="loaded"
           class="h-full w-full scale-105 object-cover group-hover:scale-100 lg:h-full lg:w-full" />
       </div>
       <div

e94f9590deba0944db7beb4fe7fe148ff7e82896
localhousee <ananditodwiky666@gmail.com>
2022-10-24 22:00:53 +0700

    add new helper for find() function

--- src/utilities/helpers.ts
+++ src/utilities/helpers.ts
@@ -1,6 +1,20 @@
-export const useSlug = (str: string): string => str.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '');
+import { ref } from "vue";
+
+export const useSlug = (str: string): string => {
+  return str.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '');
+} 
 export const useTitle = (str: string): string => {
   const regex = /(^|\b(?!(and?|at?|the|for|of|to|but|by)\b))\w+/g;
   return str.toLowerCase().replace(regex, s => s[0].toUpperCase() + s.slice(1));
 };
-export const useRedirectToAdmin = (): void => window.location.replace("/admin");
+export const useRedirectToAdmin = (): void => {
+  window.location.replace("/admin")
+};
+export function useEnsure<T> (argument: T | undefined | null, message: string = "Data not found"): T {
+  if (argument === undefined || argument === null) {
+    throw new TypeError(message);
+  }
+  return argument;
+}
+
+export const useValkyrieTypes = ref(["BIO", "MECH", "PHY", "QUA", "IMG"]);

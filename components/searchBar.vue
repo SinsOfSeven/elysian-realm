9cc3d4de884e6cabb8997be310ebff40cdd39944
localhousee <ananditodwiky666@gmail.com>
2022-11-11 06:46:13 +0700

    create searchBar component

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const search = ref("");
+</script>
 <template>
-search bar
+    <div class="py-5 flex w-full justify-center px-8">
+        <input type="text" v-model="search" class="w-full border border-green-500 rounded-full focus:outline-none focus:ring-0 focus:border-2 focus:border-green-400 text-green-700" @input="$emit('filter', search)" />
+    </div>
 </template>

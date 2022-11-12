54254da887403022e0fcaadc06c3094bfb81e2e4
localhousee <ananditodwiky666@gmail.com>
2022-11-12 15:13:04 +0700

    add description component

---
+++ components/danger.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid';
+
+defineProps<{ danger: string | Array<string> }>();
+</script>
+<template>
+    <div class="flex w-full items-center space-x-2 text-xs">
+        <div>
+            <ExclamationTriangleIcon class="w-6 h-6 text-red-500" />
+        </div>
+        <div v-if="typeof danger === 'string'">
+            <p>{{ danger }}</p>
+        </div>
+        <p v-else v-for="i in danger" :key="i">{{ i }}</p>
+    </div>
+</template>
---
+++ components/information.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ note: string | Array<string> }>();
+</script>
+<template>
+    <p v-if="typeof note === 'string'" class="text-xs py-4">{{ note }}</p>
+    <div v-else class="py-4 text-xs">
+        <p v-for="i in note" :key="i">{{ i }}</p>
+    </div>
+</template>

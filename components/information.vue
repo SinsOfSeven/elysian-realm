3cd0ee2d70efd65d6ba3ce46afa42a7b16b48fe4
localhousee <ananditodwiky666@gmail.com>
2022-11-13 02:48:15 +0700

    increase size of information

--- components/information.vue
+++ components/information.vue
@@ -2,8 +2,8 @@
 defineProps<{ note: string | Array<string> }>();
 </script>
 <template>
-    <p v-if="typeof note === 'string'" class="text-xs py-4">{{ note }}</p>
-    <div v-else class="py-4 text-xs">
-        <p v-for="i in note" :key="i">{{ i }}</p>
+    <div class="py-2 text-base">
+        <p v-if="typeof note === 'string'">{{ note }}</p>
+        <p v-else v-for="i in note" :key="i">{{ i }}</p>
     </div>
 </template>

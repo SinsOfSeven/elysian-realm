8d840401080003c08f51a0eddbb104b28ab99174
localhousee <ananditodwiky666@gmail.com>
2022-11-12 15:11:00 +0700

    add responsiveness to search bar

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -3,6 +3,6 @@ const search = ref("");
 </script>
 <template>
     <div class="py-5 flex w-full justify-center px-8">
-        <input type="text" v-model="search" class="w-full border border-green-500 rounded-full focus:outline-none focus:ring-0 focus:border-2 focus:border-green-400 text-green-700" @input="$emit('filter', search)" />
+        <input type="text" v-model="search" placeholder="Search for valkyrie" class="placeholder:text-gray-400 w-full max-w-md border border-green-500 rounded-full focus:outline-none focus:ring-0 focus:border-2 focus:border-green-400 text-green-700" @input="$emit('filter', search)" />
     </div>
 </template>

07a6324b53f6281a4cb3f5065448033bbb4e6fdf
localhousee <ananditodwiky666@gmail.com>
2022-11-13 03:00:36 +0700

    update navigation design

--- components/navigation.vue
+++ components/navigation.vue
@@ -2,14 +2,16 @@
 import { valkyries } from '~~/utilities/database';
 import { slug } from '~~/utilities/helpers';
 
+defineProps<{ selected: number }>();
+
 const name = useRoute().params.name;
 const valkyrieIndex = valkyries.findIndex(el => slug(el.name) === name);
 const valkyrie = valkyries[valkyrieIndex];
 const previousValk = valkyrieIndex > 0 ? valkyries[valkyrieIndex - 1] : valkyries[valkyries.length - 1];
-const nextValk = valkyrieIndex < valkyries.length ? valkyries[valkyrieIndex + 1] : valkyries[0];
+const nextValk = valkyrieIndex < valkyries.length - 1 ? valkyries[valkyrieIndex + 1] : valkyries[0];
 </script>
 <template>
-    <div class="sticky bottom-0 bg-gradient-to-r text-white" 
+    <div class="sticky bottom-0 bg-gradient-to-r text-white"
     :class="[
         previousValk.type === 'BIO' ? 'from-orange-600'
         : previousValk.type === 'MECH' ? 'from-blue-600'
@@ -22,12 +24,12 @@ const nextValk = valkyrieIndex < valkyries.length ? valkyries[valkyrieIndex + 1]
         : nextValk.type === 'QUA' ? 'to-violet-600'
         : 'to-[#8d6057]',
     ]">
-        <div class="flex w-full items-center">
-            <NuxtLink class="px-4" :to="`/${ slug(previousValk.name) }`">{{ previousValk.name }}</NuxtLink>
-            <div class="grid w-full grid-cols-1">
-                <button @click="$emit('selected', index)" v-for="(build, index) in valkyrie.builds" :key="index" class="px-4 py-2 border border-black">{{ build.name }}</button>
+        <div class="grid w-full grid-cols-3">
+            <NuxtLink class="px-4 w-full h-full flex justify-center items-center" :to="`/${ slug(previousValk.name) }`">{{ previousValk.name }}</NuxtLink>
+            <div class="grid w-full grid-cols-1 px-1 py-0.5">
+                <button @click="$emit('selected', index)" v-for="(build, index) in valkyrie.builds" :key="index" class="px-4 py-2 rounded" :class="selected === index ? 'border border-white border-2' : ''">{{ build.name }}</button>
             </div>
-            <NuxtLink class="px-4" :to="`/${ slug(nextValk.name) }`">{{ nextValk.name }}</NuxtLink>
+            <NuxtLink class="px-4 w-full h-full flex justify-center items-center" :to="`/${ slug(nextValk.name) }`">{{ nextValk.name }}</NuxtLink>
         </div>
     </div>
 </template>

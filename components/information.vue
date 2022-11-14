125531084ff40d9e23e2c0808fe5cfd5b7f1740a
localhousee <ananditodwiky666@gmail.com>
2022-11-14 17:48:28 +0700

    update information component

--- components/information.vue
+++ components/information.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-defineProps<{ note: string | Array<string> }>();
+defineProps<{ note: string }>();
+
+const isTruncate = ref(true);
+
+const toggleTruncate = () => {
+    isTruncate.value = !isTruncate.value;
+    if (isTruncate.value) truncateButton.value = "Read more";
+    else truncateButton.value = "Show less";
+};
+
+const truncateButton = ref("Read more");
 </script>
 <template>
     <div class="py-2 text-base">
-        <p v-if="typeof note === 'string'">{{ note }}</p>
-        <p v-else v-for="i in note" :key="i">{{ i }}</p>
+        <p>{{ isTruncate && note.length > 100 ? `${note.substring(0, 100)}...` : note }}</p>
+        <p @click="toggleTruncate" v-if="note.length > 100" class="cursor-pointer text-gray-400">{{ truncateButton }}</p>
     </div>
 </template>

d9da5c5c001cfb6dfb907251117bf6e114609c6c
localhousee <ananditodwiky666@gmail.com>
2022-11-14 17:48:03 +0700

    update danger component

--- components/danger.vue
+++ components/danger.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid';
+defineProps<{ danger: string }>();
+const isTruncate = ref(true);
 
-defineProps<{ danger: string | Array<string> }>();
+const toggleTruncate = () => {
+    isTruncate.value = !isTruncate.value;
+    if (isTruncate.value) truncateButton.value = "Show warning";
+    else truncateButton.value = "Hide warning";
+};
+
+const truncateButton = ref("Read more");
 </script>
 <template>
     <div class="flex w-full items-center space-x-2 text-base">
-        <div>
-            <ExclamationTriangleIcon class="w-6 h-6 text-red-500" />
+        <ExclamationTriangleIcon class="w-8 h-8 text-red-500" />
+        <div class="flex flex-col w-full">
+            <p>{{ !isTruncate ? danger : '' }}</p>
+            <p @click="toggleTruncate" v-if="danger" class="cursor-pointer text-gray-400">{{ truncateButton }}</p>
         </div>
-        <div v-if="typeof danger === 'string'">
-            <p>{{ danger }}</p>
-        </div>
-        <p v-else v-for="i in danger" :key="i">{{ i }}</p>
     </div>
 </template>

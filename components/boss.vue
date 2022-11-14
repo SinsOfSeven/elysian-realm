98f3e2bd9826c592a70ea2206e03c767daf54779
localhousee <ananditodwiky666@gmail.com>
2022-11-14 17:26:14 +0700

    update boss component

--- components/boss.vue
+++ components/boss.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
-import { InformationCircleIcon } from '@heroicons/vue/24/solid';
+import { InformationCircleIcon } from "@heroicons/vue/24/solid";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 interface Props {
     info: string;
     note?: string | Array<string>;
 }
-defineProps<{ boss: Props }>();
+defineProps<{ boss: Props; class: string }>();
 
-const open = ref(false);
-const toggleModal = (state: boolean) => {
-    open.value = state;
-}
 </script>
 <template>
-    <span class="uppercase text-sm">boss</span>
-    <span class="text-sm font-thin">{{ boss.info }}
-        <button @click="open = true">
-            <InformationCircleIcon v-if="boss.note" class="w-6 h-6 inline-flex animate-pulse" />
-        </button>
-    </span>
-
-    <Modal @is-open="toggleModal" :open="open">
-        {{ boss.note }}
-    </Modal>
+    <div :class="class" class="flex flex-col">
+        <span class="uppercase text-sm font-thin">boss</span>
+        <Popover>
+            <PopoverButton class="uppercase text-sm flex" :class="boss.note === '' ? 'cursor-arrow': 'cursor-auto'">
+                <span class="inline-flex text-left font-bold">{{ boss.info }}</span> <InformationCircleIcon v-show="boss.note" class="ml-2 w-6 h-6 animate-pulse" />
+            </PopoverButton>
+        
+            <PopoverPanel class="absolute z-30 bg-gray-900 rounded px-4 py-2 w-sm" v-show="boss.note">
+                {{ boss.note }}
+            </PopoverPanel>
+        </Popover>
+    </div>
 </template>

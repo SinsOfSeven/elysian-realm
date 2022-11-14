21d0f8f5346232b64c3fd1d9e8e16b089f1600a4
localhousee <ananditodwiky666@gmail.com>
2022-11-14 18:42:08 +0700

    mix every component

--- pages/[name].vue
+++ pages/[name].vue
@@ -16,19 +16,38 @@ const selected = (index: number) => {
     selectedIndex.value = index;
     selectedBuild.value = valkyrie.builds[selectedIndex.value];
 };
+
+const timeline = ["Early", "Mid", "Late"];
+const time = ref(0);
+const timeText = ref(timeline[0]);
+const sigil = ref(selectedBuild.value.sigil.lists[time.value]);
+const support = ref(selectedBuild.value.support.lists[time.value]);
+
+function changeTime() {
+    time.value = (time.value + 1) % timeline.length;
+    timeText.value = timeline[time.value];
+
+    sigil.value = selectedBuild.value.sigil.lists[time.value];
+    support.value = selectedBuild.value.support.lists[time.value];
+}
 </script>
 
 <template>
     <div class="px-2 pt-8 text-white bg-gray-700">
         <p class="text-xs uppercase">{{ valkyrie.name }}</p>
         <p class="text-xl font-bold uppercase">{{ selectedBuild.name }}</p>
-        <Information v-if="selectedBuild.note" :note="selectedBuild.note" />
-        <Danger v-if="selectedBuild.danger" :danger="selectedBuild.danger" />
-        <div class="flex flex-col py-6">
-            <Boss :boss="selectedBuild.boss" />
-        </div>
-        <div class="flex w-full space-x-2 items-center">
-            <Buff v-if="selectedBuild.buff" :buff="selectedBuild.buff" />
+        <div class="flex flex-col space-y-2">
+            <Information :note="selectedBuild.note" />
+            <Danger v-if="selectedBuild.danger" :danger="selectedBuild.danger" />
+            <Boss :boss="selectedBuild.boss" class="pt-5 pb-3" />
+            <Buff :buff="selectedBuild.buff" />
+            <div class="flex w-full space-x-3">
+                <button @click="changeTime" class="text-xs bg-white text-gray-700 rounded-full w-16 h-16">
+                    {{ timeline[time] }}
+                </button>
+                <Sigil :sigil="sigil" :key="time" :note="selectedBuild.sigil.note" />
+                <Support :support="support" :key="time" :note="selectedBuild.support.note" />
+            </div>
         </div>
     </div>
     <Navigation :selected="selectedIndex" @selected="selected" />

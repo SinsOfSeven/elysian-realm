a872253e872dea6985705cba0c1a6125a90c91ad
localhousee <ananditodwiky666@gmail.com>
2022-11-15 20:13:53 +0700

    add new custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import tailwindForms from "@tailwindcss/forms";
 import tailwindAspectRatio from "@tailwindcss/aspect-ratio";
 
 export default <Partial<Config>>{
-	theme: {},
+	theme: {
+		extend: {
+			colors: {
+				"light-yellow": "#F7F6CF",
+				turquoise: "#B6D8F2",
+				"dark-pink": "#F4CFDF",
+				"dark-blue": "#5784BA",
+				"sky-blue": "#9AC8EB"
+			}
+		}
+	},
 	content: [
 		"./components/**/*.{vue,js}",
 		"./pages/**/*.vue",

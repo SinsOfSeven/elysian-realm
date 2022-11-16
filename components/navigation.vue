f9c4e30e5dcb8df643024fdd5e6d9baf2bcafa43
localhousee <ananditodwiky666@gmail.com>
2022-11-16 23:29:04 +0700

    update navigation component

--- components/navigation.vue
+++ components/navigation.vue
@@ -11,25 +11,13 @@ const previousValk = valkyrieIndex > 0 ? valkyries[valkyrieIndex - 1] : valkyrie
 const nextValk = valkyrieIndex < valkyries.length - 1 ? valkyries[valkyrieIndex + 1] : valkyries[0];
 </script>
 <template>
-    <div class="sticky bottom-0 bg-gradient-to-r text-white"
-    :class="[
-        previousValk.type === 'BIO' ? 'from-orange-600'
-        : previousValk.type === 'MECH' ? 'from-blue-600'
-        : previousValk.type === 'PHY' ? 'from-pink-600'
-        : previousValk.type === 'QUA' ? 'from-violet-600'
-        : 'from-[#8d6057]',
-        nextValk.type === 'BIO' ? 'to-orange-600'
-        : nextValk.type === 'MECH' ? 'to-blue-600'
-        : nextValk.type === 'PHY' ? 'to-pink-600'
-        : nextValk.type === 'QUA' ? 'to-violet-600'
-        : 'to-[#8d6057]',
-    ]">
+    <div class="sticky bottom-0 bg-dark-pink text-dark-blue font-semibold py-4 border border-4 border-t-dark-blue">
         <div class="grid w-full grid-cols-3">
-            <NuxtLink class="px-4 w-full h-full flex justify-center items-center" :to="`/${ slug(previousValk.name) }`">{{ previousValk.name }}</NuxtLink>
+            <NuxtLink class="px-4 w-full h-full flex justify-center items-center" :to="`/valkyrie/${ slug(previousValk.name) }`">{{ previousValk.name }}</NuxtLink>
             <div class="grid w-full grid-cols-2 place-content-around px-1 py-0.5">
-                <button @click="$emit('selected', index)" v-for="(build, index) in valkyrie.builds" :key="index" class="px-4 py-2 rounded" :class="selected === index ? 'border border-white border-2' : ''">{{ index + 1 }}</button>
+                <button @click="$emit('selected', index)" v-for="(build, index) in valkyrie.builds" :key="index" class="px-4 py-2 rounded" :class="selected === index ? 'border border-dark-blue border-2' : ''">{{ index + 1 }}</button>
             </div>
-            <NuxtLink class="px-4 w-full h-full flex justify-center items-center" :to="`/${ slug(nextValk.name) }`">{{ nextValk.name }}</NuxtLink>
+            <NuxtLink class="px-4 w-full h-full flex justify-center items-center" :to="`/valkyrie/${ slug(nextValk.name) }`">{{ nextValk.name }}</NuxtLink>
         </div>
     </div>
 </template>

4fd18c48ba38c4f39d8af884abec7c1cd20c1432
localhousee <ananditodwiky666@gmail.com>
2022-11-16 23:39:54 +0700

    update support component

--- components/support.vue
+++ components/support.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { Support } from "~~/utilities/types";
+import { ensure } from "~~/utilities/helpers";
+import { supports } from "~~/utilities/database";
+
 const props = defineProps<{ support: Support }>();
 
 const first = ref(props.support.first.split(' / '));
@@ -10,7 +13,7 @@ const second = ref(props.support.second.split(' / '));
 const secondSection = ref(0);
 const nextSecondSection = second.value.length > 1 ? ref(1) : ref(0);
 
-const swapSupport = (support: string) => {
+function swapSupport(support: string) {
     if (support === 'first') {
         firstSection.value = (firstSection.value + 1) % first.value.length;
         nextFirstSection.value = (firstSection.value + 1) % first.value.length;
@@ -18,21 +21,23 @@ const swapSupport = (support: string) => {
         secondSection.value = (secondSection.value + 1) % second.value.length;
         nextSecondSection.value = (nextSecondSection.value + 1) % second.value.length;
     }
-}
+};
+
+const fetchDescription = (name: string) => ensure(supports.find(el => el.abbr.toLowerCase() === name.toLowerCase()));
 </script>
 <template>
     <div class="flex space-x-2 w-40">
-        <button @click="first.length > 1 ? swapSupport('first') : ''" class="relative w-full">
-            <img class="absolute border border-white w-16 h-16 z-20 left-0 top-0"
+        <button @click="first.length > 1 ? swapSupport('first') : ''" class="relative w-full" @mouseenter="$emit('is-hover', true, fetchDescription(first[firstSection]))" @mouseleave="$emit('is-hover', false)">
+            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0"
                 :src="`/supports/${first[firstSection]}.webp`" :alt="first[firstSection]" :key="firstSection">
-            <img class="absolute border border-white w-16 h-16 z-0 left-4 top-4" v-show="first.length > 1"
+            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="first.length > 1"
                 :src="`/supports/${first[nextFirstSection]}.webp`" :alt="first[nextFirstSection]"
                 :key="nextFirstSection">
         </button>
-        <button @click="second.length > 1 ? swapSupport('second') : ''" class="relative w-full">
-            <img class="absolute border border-white w-16 h-16 z-20 left-0 top-0"
+        <button @click="second.length > 1 ? swapSupport('second') : ''" class="relative w-full" @mouseenter="$emit('is-hover', true, fetchDescription(second[secondSection]))" @mouseleave="$emit('is-hover', false)">
+            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0"
                 :src="`/supports/${second[secondSection]}.webp`" :alt="second[secondSection]" :key="secondSection">
-            <img class="absolute border border-white w-16 h-16 z-0 left-4 top-4" v-show="second.length > 1"
+            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="second.length > 1"
                 :src="`/supports/${second[nextSecondSection]}.webp`" :alt="second[nextSecondSection]"
                 :key="nextSecondSection">
         </button>

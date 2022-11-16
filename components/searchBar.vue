c8ff357b39975ee6c1af3d6530b9b58b38bf90fc
localhousee <ananditodwiky666@gmail.com>
2022-11-16 23:29:49 +0700

    update search component

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -3,6 +3,6 @@ const search = ref("");
 </script>
 <template>
     <div class="py-5 flex w-full justify-center px-8">
-        <input type="text" v-model="search" placeholder="Search for valkyrie" class="placeholder:text-gray-400 w-full max-w-md border border-green-500 rounded-full focus:outline-none focus:ring-0 focus:border-2 focus:border-green-400 text-green-700" @input="$emit('filter', search)" />
+        <input type="text" v-model="search" placeholder="Search for valkyrie" class="placeholder:text-gray-400 text-dark-blue w-full max-w-md rounded-full border-transparent focus:outline-none focus:ring-0 focus:border focus:border-dark-blue" @input="$emit('filter', search)" />
     </div>
 </template>

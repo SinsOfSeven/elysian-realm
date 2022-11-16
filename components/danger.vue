ea22567e3323aef0edff26072cef731547545788
localhousee <ananditodwiky666@gmail.com>
2022-11-16 23:43:32 +0700

    update danger component

--- components/danger.vue
+++ components/danger.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
-import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid';
 defineProps<{ danger: string }>();
 const isTruncate = ref(true);
 
 const toggleTruncate = () => {
     isTruncate.value = !isTruncate.value;
-    if (isTruncate.value) truncateButton.value = "Show warning";
-    else truncateButton.value = "Hide warning";
+    if (isTruncate.value) truncateButton.value = "Read more";
+    else truncateButton.value = "Show less";
 };
 
 const truncateButton = ref("Read more");
 </script>
 <template>
-    <div class="flex w-full items-center space-x-2 text-base">
-        <ExclamationTriangleIcon class="w-8 h-8 text-red-500" />
+    <div class="flex flex-col w-full lg:w-1/2 items-center space-x-2 text-base">
+        <p class="flex text-red-500 text-left w-full font-bold">Warning!</p>
         <div class="flex flex-col w-full">
-            <p>{{ !isTruncate ? danger : '' }}</p>
-            <p @click="toggleTruncate" v-if="danger" class="cursor-pointer text-gray-400">{{ truncateButton }}</p>
+            <p>{{ isTruncate && danger.length > 100 ? `${danger.substring(0,100)}...` : danger }}</p>
+            <p @click="toggleTruncate" v-if="danger" class="cursor-pointer text-light-yellow animate-pulse underline">{{ truncateButton }}</p>
         </div>
     </div>
 </template>

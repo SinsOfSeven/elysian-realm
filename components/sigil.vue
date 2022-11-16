bdc075aee5477dec53bc3c6c88a745a03d61ce3b
localhousee <ananditodwiky666@gmail.com>
2022-11-16 23:30:58 +0700

    update sigil component

--- components/sigil.vue
+++ components/sigil.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { Sigil } from "~~/utilities/types";
-import { slug } from "~~/utilities/helpers";
+import { slug, ensure } from "~~/utilities/helpers";
+import { sigils } from "~~/utilities/database";
+
 const props = defineProps<{ sigil: Sigil }>();
 
 const first = ref(props.sigil.first.split(' / '));
@@ -11,7 +13,7 @@ const second = ref(props.sigil.second.split(' / '));
 const secondSection = ref(0);
 const nextSecondSection = second.value.length > 1 ? ref(1) : ref(0);
 
-const swapSigil = (sigil: string) => {
+function swapSigil(sigil: string) {
     if (sigil === 'first') {
         firstSection.value = (firstSection.value + 1) % first.value.length;
         nextFirstSection.value = (firstSection.value + 1) % first.value.length;
@@ -19,17 +21,21 @@ const swapSigil = (sigil: string) => {
         secondSection.value = (secondSection.value + 1) % second.value.length;
         nextSecondSection.value = (nextSecondSection.value + 1) % second.value.length;
     }
-}
+};
+
+const fetchDescription = (name: string) => ensure(sigils.find(el => el.name.toLowerCase() === name.toLowerCase()));
 </script>
 <template>
-    <div class="flex space-x-2 w-40">
-        <button @click="first.length > 1 ? swapSigil('first') : ''" class="relative w-full">
-            <img class="absolute border border-white w-16 h-16 z-20 left-0 top-0" :src="`/sigils/${slug(first[firstSection])}.webp`" :alt="first[firstSection]" :key="firstSection">
-            <img class="absolute border border-white w-16 h-16 z-0 left-4 top-4" v-show="first.length > 1" :src="`/sigils/${slug(first[nextFirstSection])}.webp`" :alt="first[nextFirstSection]" :key="nextFirstSection">
-        </button>
-        <button @click="second.length > 1 ? swapSigil('second') : ''" class="relative w-full">
-            <img class="absolute border border-white w-16 h-16 z-20 left-0 top-0" :src="`/sigils/${slug(second[secondSection])}.webp`" :alt="second[secondSection]" :key="secondSection">
-            <img class="absolute border border-white w-16 h-16 z-0 left-4 top-4" v-show="second.length > 1" :src="`/sigils/${slug(second[nextSecondSection])}.webp`" :alt="second[nextSecondSection]" :key="nextSecondSection">
-        </button>
+    <div class="w-40 relative">
+        <div class="flex space-x-2">
+            <button @click="first.length > 1 ? swapSigil('first') : ''" class="relative w-full" @mouseenter="$emit('isHover', true, fetchDescription(first[firstSection]))" @mouseleave="$emit('isHover', false)">
+                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0" :src="`/sigils/${slug(first[firstSection])}.webp`" :alt="first[firstSection]" :key="firstSection">
+                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="first.length > 1" :src="`/sigils/${slug(first[nextFirstSection])}.webp`" :alt="first[nextFirstSection]" :key="nextFirstSection">
+            </button>
+            <button @click="second.length > 1 ? swapSigil('second') : ''" class="relative w-full" @mouseenter="$emit('isHover', true, fetchDescription(second[secondSection]))" @mouseleave="$emit('isHover', false)">
+                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0" :src="`/sigils/${slug(second[secondSection])}.webp`" :alt="second[secondSection]" :key="secondSection">
+                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="second.length > 1" :src="`/sigils/${slug(second[nextSecondSection])}.webp`" :alt="second[nextSecondSection]" :key="nextSecondSection">
+            </button>
+        </div>
     </div>
 </template>

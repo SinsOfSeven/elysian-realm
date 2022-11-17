9bfcdc7c92c46bfa0bf0c2f1772e3aa995c9894e
localhousee <ananditodwiky666@gmail.com>
2022-11-17 13:10:39 +0700

    improve readability on placeholder

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -3,6 +3,6 @@ const search = ref("");
 </script>
 <template>
     <div class="py-5 flex w-full justify-center px-8">
-        <input type="text" v-model="search" placeholder="Search for valkyrie" class="placeholder:text-gray-400 text-dark-blue w-full max-w-md rounded-full border-transparent focus:outline-none focus:ring-0 focus:border focus:border-dark-blue" @input="$emit('filter', search)" />
+        <input type="text" v-model="search" placeholder="Search for valkyrie" class="text-dark-blue w-full max-w-md rounded-full border-transparent focus:outline-none focus:ring-0 focus:border focus:border-dark-blue" @input="$emit('filter', search)" />
     </div>
 </template>

4a8eaa7934ab24280c97d81c7829720d8e461159
localhousee <ananditodwiky666@gmail.com>
2022-11-20 17:05:16 +0700

    removing unused props and move Meta to template

--- utilities/helpers.ts
+++ utilities/helpers.ts
@@ -5,7 +5,7 @@ export function slug(str: string): string {
 		.replace(/[^\w\s-]/g, "")
 		.replace(/[\s_-]+/g, "-")
 		.replace(/^-+|-+$/g, "");
-};
+}
 
 export function ensure<T>(
 	argument: T | undefined | null,
@@ -16,7 +16,23 @@ export function ensure<T>(
 	}
 	return argument;
 }
-export function teamBuild(valk: string, rank: string, weap: string, stigmata: Array<string>): string {
+export function teamBuild(
+	valk: string,
+	rank: string,
+	weap: string,
+	stigmata: Array<string>
+): string {
 	const url = "https://www.arustats.com/en-us/hi3/team-builder";
 	return `${url}?team_1_valkyrie=${valk}&team_1_valkyrie_rank=${rank}&team_1_weapon=${weap}&team_1_stigma_top=${stigmata[0]}&team_1_stigma_mid=${stigmata[1]}&team_1_stigma_bottom=${stigmata[2]}&elf=&elf_stars=2.0`;
 }
+
+import { valkyries } from "../utilities/database";
+
+export const valkyrieList = valkyries.map((el) => {
+	const filtered = { name: "", keyword: "" };
+
+	filtered.name = el.name;
+	filtered.keyword = el.keyword;
+
+	return filtered;
+});

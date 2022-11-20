e3e9f0bbdb59775cf5513c6f9e5a4ef177ef5dc2
localhousee <ananditodwiky666@gmail.com>
2022-11-21 01:10:30 +0700

    use popover for showing notes

--- components/sigil.vue
+++ components/sigil.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 import { Sigil } from "~~/utilities/types";
 import { slug, ensure } from "~~/utilities/helpers";
 import { sigils } from "~~/utilities/database";
 
-const props = defineProps<{ sigil: Sigil }>();
+const props = defineProps<{ sigil: Sigil, note?: string }>();
 
 const first = ref(props.sigil.first.split(' / '));
 const firstSection = ref(0);
@@ -26,16 +27,57 @@ function swapSigil(sigil: string) {
 const fetchDescription = (name: string) => ensure(sigils.find(el => el.name.toLowerCase() === name.toLowerCase()));
 </script>
 <template>
-    <div class="w-40 relative">
-        <div class="flex space-x-2">
-            <button @click="first.length > 1 ? swapSigil('first') : ''" class="relative w-full" @mouseenter="$emit('isHover', true, fetchDescription(first[firstSection]))" @mouseleave="$emit('isHover', false)">
-                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0" :src="`/sigils/${slug(first[firstSection])}.webp`" :alt="first[firstSection]" :key="firstSection">
-                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="first.length > 1" :src="`/sigils/${slug(first[nextFirstSection])}.webp`" :alt="first[nextFirstSection]" :key="nextFirstSection">
-            </button>
-            <button @click="second.length > 1 ? swapSigil('second') : ''" class="relative w-full" @mouseenter="$emit('isHover', true, fetchDescription(second[secondSection]))" @mouseleave="$emit('isHover', false)">
-                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0" :src="`/sigils/${slug(second[secondSection])}.webp`" :alt="second[secondSection]" :key="secondSection">
-                <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="second.length > 1" :src="`/sigils/${slug(second[nextSecondSection])}.webp`" :alt="second[nextSecondSection]" :key="nextSecondSection">
-            </button>
-        </div>
+    <div class="flex w-full justify-center">
+        <button @click="first.length > 1 ? swapSigil('first') : ''" class="w-24 flex relative z-0"
+            @mouseenter="$emit('isHover', true, fetchDescription(first[firstSection]))"
+            @mouseleave="$emit('isHover', false)">
+            <nuxt-img class="absolute top-0 left-0 z-10 border border-black bg-yellow-100"
+                :src="`/sigils/${slug(first[firstSection])}.webp`" :alt="first[firstSection]" :key="firstSection"
+                placeholder height="64" width="64" />
+            <nuxt-img class="absolute top-2 left-2 z-0 border border-black bg-yellow-100" v-show="first.length > 1"
+                :src="`/sigils/${slug(first[nextFirstSection])}.webp`" :alt="first[nextFirstSection]"
+                :key="nextFirstSection" placeholder height="64" width="64" />
+        </button>
+        <button @click="second.length > 1 ? swapSigil('second') : ''" class="w-24 flex relative z-0"
+            @mouseenter="$emit('isHover', true, fetchDescription(second[secondSection]))"
+            @mouseleave="$emit('isHover', false)">
+            <nuxt-img class="absolute top-0 left-0 z-10 border border-black bg-yellow-100"
+                :src="`/sigils/${slug(second[secondSection])}.webp`" :alt="second[secondSection]" :key="secondSection"
+                placeholder height="64" width="64" />
+            <nuxt-img class="absolute top-2 left-2 z-0 border border-black bg-yellow-100" v-show="second.length > 1"
+                :src="`/sigils/${slug(second[nextSecondSection])}.webp`" :alt="second[nextSecondSection]"
+                :key="nextSecondSection" placeholder height="64" width="64" />
+        </button>
+        <Popover>
+            <PopoverButton class="text-left focus:outline-none">
+                <!-- InformationCircleIcon -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    class="w-12 h-12" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                </svg>
+            </PopoverButton>
+            <Transition name="slide-fade">
+                <PopoverPanel class="z-20 rounded overflow-y-auto bg-dark-pink text-white w-full absolute left-0 mt-6 px-3 py-2 border border-white">
+                    <p v-if="note">{{ note }}</p>
+                    <p v-else>No additional information</p>
+                </PopoverPanel>
+            </Transition>
+        </Popover>
     </div>
 </template>
+<style>
+.slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+}
+</style>
--- components/support.vue
+++ components/support.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 import { Support } from "~~/utilities/types";
 import { ensure } from "~~/utilities/helpers";
 import { supports } from "~~/utilities/database";
 
-const props = defineProps<{ support: Support }>();
+const props = defineProps<{ support: Support, note?: string, danger?: string }>();
 
 const first = ref(props.support.first.split(' / '));
 const firstSection = ref(0);
@@ -26,20 +27,44 @@ function swapSupport(support: string) {
 const fetchDescription = (name: string) => ensure(supports.find(el => el.abbr.toLowerCase() === name.toLowerCase()));
 </script>
 <template>
-    <div class="flex space-x-2 w-40">
-        <button @click="first.length > 1 ? swapSupport('first') : ''" class="relative w-full" @mouseenter="$emit('is-hover', true, fetchDescription(first[firstSection]))" @mouseleave="$emit('is-hover', false)">
-            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0"
-                :src="`/supports/${first[firstSection]}.webp`" :alt="first[firstSection]" :key="firstSection">
-            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="first.length > 1"
-                :src="`/supports/${first[nextFirstSection]}.webp`" :alt="first[nextFirstSection]"
-                :key="nextFirstSection">
-        </button>
-        <button @click="second.length > 1 ? swapSupport('second') : ''" class="relative w-full" @mouseenter="$emit('is-hover', true, fetchDescription(second[secondSection]))" @mouseleave="$emit('is-hover', false)">
-            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-20 left-0 top-0"
-                :src="`/supports/${second[secondSection]}.webp`" :alt="second[secondSection]" :key="secondSection">
-            <img class="absolute border border-dark-blue bg-light-yellow w-16 h-16 z-0 left-2 top-2" v-show="second.length > 1"
-                :src="`/supports/${second[nextSecondSection]}.webp`" :alt="second[nextSecondSection]"
-                :key="nextSecondSection">
-        </button>
-    </div>
-</template>
+<div class="flex w-full justify-center py-8">
+    <button @click="first.length > 1 ? swapSupport('first') : ''" class="w-24 flex relative z-0"
+        @mouseenter="$emit('isHover', true, fetchDescription(first[firstSection]))"
+        @mouseleave="$emit('isHover', false)">
+        <nuxt-img class="absolute top-0 left-0 z-10 border border-black bg-yellow-100"
+            :src="`/supports/${first[firstSection]}.webp`" :alt="first[firstSection]" :key="firstSection"
+            placeholder height="64" width="64" />
+        <nuxt-img class="absolute top-2 left-2 z-0 border border-black bg-yellow-100" v-show="first.length > 1"
+            :src="`/supports/${first[nextFirstSection]}.webp`" :alt="first[nextFirstSection]"
+            :key="nextFirstSection" placeholder height="64" width="64" />
+    </button>
+    <button @click="second.length > 1 ? swapSupport('second') : ''" class="w-24 flex relative z-0"
+        @mouseenter="$emit('isHover', true, fetchDescription(second[secondSection]))"
+        @mouseleave="$emit('isHover', false)">
+        <nuxt-img class="absolute top-0 left-0 z-10 border border-black bg-yellow-100"
+            :src="`/supports/${second[secondSection]}.webp`" :alt="second[secondSection]" :key="secondSection"
+            placeholder height="64" width="64" />
+        <nuxt-img class="absolute top-2 left-2 z-0 border border-black bg-yellow-100" v-show="second.length > 1"
+            :src="`/supports/${second[nextSecondSection]}.webp`" :alt="second[nextSecondSection]"
+            :key="nextSecondSection" placeholder height="64" width="64" />
+    </button>
+    <Popover>
+        <PopoverButton class="text-left focus:outline-none">
+            <!-- InformationCircleIcon -->
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-12 h-12" v-show="note" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            </svg>
+        </PopoverButton>
+        <Transition name="slide-fade">
+            <PopoverPanel class="z-20 rounded overflow-y-auto bg-dark-pink text-white w-full absolute left-0 mt-6 px-3 py-2 border border-white">
+                <p v-if="note">{{ note }}</p>
+                <p v-else>
+                    <p v-if="danger"><span class="font-semibold uppercase">Warning!</span><br /> {{ danger }}</p>
+                    <p v-else>No additional information</p>
+                </p>
+            </PopoverPanel>
+        </Transition>
+    </Popover>
+</div></template>

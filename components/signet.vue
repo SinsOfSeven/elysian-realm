3c29129f0efa5ef7eff2449aa9a1a9898f1fb9a5
localhousee <ananditodwiky666@gmail.com>
2022-11-21 13:26:43 +0700

    add icon for signet list

--- components/signet.vue
+++ components/signet.vue
@@ -67,8 +67,14 @@ const isOpen = ref(false);
             <table class="w-full border border-collapse">
                 <tr v-for="(item, index) in selectedSignet.lists" :key="index">
                     <td class="border border-white py-3">
-                        <button @click="descriptionModal = item.description; isOpen = true" class="flex">
-                            <span class="inline-flex text-left pl-3 animate-pulse">{{ item.name }}</span>
+                        <button @click="descriptionModal = item.description; isOpen = true" class="flex items-center space-x-2">
+                            <span class="inline-flex text-left pl-3">{{ item.name }}</span>
+                            <!-- InformationCircleIcon -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                class="w-8 h-8" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                            </svg>
                         </button>
                     </td>
                     <td class="border border-white text-center">{{ item.priority }}</td>

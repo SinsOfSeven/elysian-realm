814c72eca805cf2ee1c64bb178c6b6dbf7289809
localhousee <ananditodwiky666@gmail.com>
2022-11-21 13:25:45 +0700

    update design on md screen

--- components/setup.vue
+++ components/setup.vue
@@ -25,20 +25,24 @@ const gear = computed(() => {
 
 </script>
 <template>
-    <div class="bg-gradient-to-b from-dark-blue via-dark-violet to-dark-pink text-white w-full flex flex-col md:flex-row px-6 py-4 space-y-4">
-        <div class="flex flex-col w-full lg:w-3/12 lg:text-lg justify-center text-center">
-            <p class="text-center font-semibold">{{ selectedSetup.rank }} - {{ selectedSetup.difficult }}</p>
-            <p class="py-2">{{ selectedSetup.weap }}</p>
-            <ul class="list-inside">
-                <li v-for="(item, index) in selectedSetup.stigmata" :key="index">{{ item }}</li>
-            </ul>
-            <NuxtLink :to="gear" class="py-2 underline font-semibold text-lg" target="_blank" rel="noopener noreferrer">Visual preview by Arustats</NuxtLink>
-            <NuxtLink class="underline text-lg font-semibold animate-pulse" :to="selectedSetup.ref">
-                Reference
-            </NuxtLink>
+    <div class="md:mt-24 bg-gradient-to-b from-dark-blue via-dark-violet to-dark-pink text-white w-full flex flex-col md:flex-row px-6 py-4 space-y-4 md:space-y-0">
+        <div class="flex flex-col md:flex-row w-full lg:w-3/12 lg:text-lg justify-center text-center">
+            <div class="flex flex-col w-full">
+                <p class="text-center font-semibold">{{ selectedSetup.rank }} - {{ selectedSetup.difficult }}</p>
+                <p class="py-2">{{ selectedSetup.weap }}</p>
+                <ul class="list-inside">
+                    <li v-for="(item, index) in selectedSetup.stigmata" :key="index">{{ item }}</li>
+                </ul>
+            </div>
+            <div class="flex flex-col w-full">
+                <NuxtLink :to="gear" class="py-2 underline font-semibold text-lg" target="_blank" rel="noopener noreferrer">Visual preview by Arustats</NuxtLink>
+                <NuxtLink class="underline text-lg font-semibold" :to="selectedSetup.ref" target="_blank" rel="noopener noreferrer">
+                    Reference
+                </NuxtLink>
+            </div>
         </div>
-        <div class="pt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-start lg:text-lg w-full lg:w-3/12 text-sm place-content-start">
-            <button @click="selected(index)" v-for="(item, index) in setup.lists" :key="index" :class="['w-full border border-dark-blue rounded max-w-sm px-4 font-semibold h-12', index === selectedIndex ? 'bg-dark-blue text-white' : '']">
+        <div class="pt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-start lg:text-lg w-full max-w-sm lg:w-3/12 text-sm place-content-start gap-2">
+            <button @click="selected(index)" v-for="(item, index) in setup.lists" :key="index" :class="['w-full rounded px-4 font-semibold h-12', index === selectedIndex ? 'bg-dark-blue text-white border border-white animate-pulse' : 'bg-white text-dark-blue']">
                 {{ item.time }}
             </button>
         </div>

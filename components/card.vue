5bd97a264530aebcfc843c47b4a14aaab835fd2c
localhousee <ananditodwiky666@gmail.com>
2022-11-21 16:16:56 +0700

    fix aspect ratio of valk image card

--- components/card.vue
+++ components/card.vue
@@ -11,7 +11,7 @@ defineProps<{ valkyrie: Props }>();
     <NuxtLink :to="`/${slug(valkyrie.name)}`" class="group relative" :id="slug(valkyrie.name)">
         <div class="h-full w-full bg-sky-200 rounded-lg aspect-w-9 aspect-h-16 overflow-hidden border border-white">
             <nuxt-img :alt="valkyrie.name" :src="`/valkyries/${slug(valkyrie.name)}.jpg`" class="scale-105 group-hover:scale-100 group-hover:opacity-90"
-             width="270" height="480" format="webp" :placeholder="[50, 100, 10]" />
+             width="270" height="480" format="webp" :placeholder="[50, 100, 10]" fit="cover" />
         </div>
         <div class="absolute bottom-0 w-full py-2 px-4 rounded-b-lg text-sm md:text-base uppercase text-white bg-gradient-to-r from-dark-blue to-dark-violet border border-white">{{ valkyrie.name }}</div>
     </NuxtLink>
